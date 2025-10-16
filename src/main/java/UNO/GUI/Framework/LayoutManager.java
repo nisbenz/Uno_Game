621321a3805e346d910daf33aadeae4e60015c5b
Nisbenz <benazzaanis783@gmail.com>
2025-10-16 21:54:20 +0100

    Delete src/main/java/UNO/GUI directory

--- src/main/java/UNO/GUI/Framework/LayoutManager.java
+++
@@ -1,9 +0,0 @@
-package UNO.GUI.Framework;
-
-/**
- * Strategy for laying out the children of a Container.
- */
-public interface LayoutManager {
-    /** Position and size each child in the parent container. */
-    void layoutContainer(Container parent);
-}
